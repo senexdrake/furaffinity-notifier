6de7fab02355969fdda94befc7ad0dbc356d1b3f
Fabian Wünderich <fabian@wuenderich.de>
2024-07-22 14:00:32 +0200

    Separate message templates into it's own file

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -17,30 +17,6 @@ import (
 	"time"
 )
 
-var newNoteMessageTemplate = util.TrimHtmlText(`
-New note on FurAffinity from <a href="%s">%s</a>!
----------------------------------
-<b>%s</b>
-
-%s
----------------------------------
-<a href="%s">Open</a>
-(Note ID: <code>%d</code>)
-`)
-
-var privacyPolicyTemplate = util.TrimHtmlText(`
-This bot saves the following user information:
-
-1. Your Chat ID (to match your watched Patreon Rewards to your Telegram account)
-In your case, this would be <code>%d</code>
-2. Your provided user information:
-	- Unread notes setting
-3. Your FurAffinity cookies 
-	- these are very sensitive, this allows the bot to fully impersonate you, which is required due to how FurAffinity works
-4. A list of Note IDs that belong to your FurAffinity account
-	- this is needed to keep track of notes this bot has notified you about already. No content is stored.
-`)
-
 var botInstance *bot.Bot
 var botContext context.Context
 var botContextCancel context.CancelFunc
---
+++ internal/telegram/templates.go
@@ -0,0 +1,30 @@
+package telegram
+
+import "github.com/senexdrake/furaffinity-notifier/internal/util"
+
+var newNoteMessageTemplate = util.TrimHtmlText(`
+New note on FurAffinity from <a href="%s">%s</a>!
+---------------------------------
+<b>%s</b>
+
+%s
+---------------------------------
+<a href="%s">Open</a>
+(Note ID: <code>%d</code>)
+`)
+
+var privacyPolicyTemplate = util.TrimHtmlText(`
+This bot saves the following user information:
+
+1. Your Chat ID (to identify you and match your data to your Telegram account)
+	- In your case, this would be <code>%d</code>
+
+2. Your provided user information:
+	- Unread notes setting
+
+3. Your FurAffinity cookies 
+	- these are very sensitive, this allows the bot to fully impersonate you, which is required due to how FurAffinity works
+
+4. A list of Note IDs that belong to your FurAffinity account
+	- this is needed to keep track of notes this bot has notified you about already. No content is stored.
+`)
