d7014f1eee1306e29ea314571fb67cca3a8fdf26
Fabian Wünderich <fabian@wuenderich.de>
2025-10-27 00:12:21 +0100

    Temporarily limit submission entries to user "elkaart"

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func updateForUser(user *db.User, doneCallback func()) {
 	entryTypes := user.EnabledEntryTypes()
 
 	if slices.Contains(entryTypes, entries.EntryTypeSubmission) {
-		for submission := range c.GetNewSubmissionEntries() {
+		for submission := range c.GetNewSubmissionEntriesFromUsers("elkaart") {
 			telegram.HandleNewSubmission(submission, user)
 		}
 	}
